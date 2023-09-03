efc639f6a02643c33096c650ea0a4694030d2090
Khoza1 <felixjuliano3d@gmail.com>
2023-09-03 17:23:46 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,81 +1,89 @@
-import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,TextInput,Button,TouchableOpacity } from 'react-native';
 
-interface State {
-  displayValue: string;
-}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.testo} >KHOZA DEV</Text>
+      <StatusBar style="light" />
+      <View style={styles.cartao}>
+        <View>
+          <Text>Usuario:</Text>
+          <TextInput style={styles.input}></TextInput>
+        </View>
 
-class CalculatorApp extends Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      displayValue: '0',
-    };
-  }
+        <View>
+          <Text>Email:</Text>
+          <TextInput keyboardType='email-address' style={styles.input}></TextInput>
+        </View>
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.display}>{this.state.displayValue}</Text>
-        <View style={styles.buttonRow}>
-          <Button title="1" onPress={() => this.handleInput('1')} />
-          <Button title="2" onPress={() => this.handleInput('2')} />
-          <Button title="3" onPress={() => this.handleInput('3')} />
+        <View>
+          <Text>Senha:</Text>
+          <TextInput secureTextEntry={true} style={styles.input}></TextInput>
         </View>
-        <View style={styles.buttonRow}>
-          <Button title="4" onPress={() => this.handleInput('4')} />
-          <Button title="5" onPress={() => this.handleInput('5')} />
-          <Button title="6" onPress={() => this.handleInput('6')} />
+
+        <View>
+          <Text>Confirme Senha:</Text>
+          <TextInput secureTextEntry={true} style={styles.input}></TextInput>
         </View>
-        <View style={styles.buttonRow}>
-          <Button title="7" onPress={() => this.handleInput('7')} />
-          <Button title="8" onPress={() => this.handleInput('8')} />
-          <Button title="9" onPress={() => this.handleInput('9')} />
-        </View>
-        <View style={styles.buttonRow}>
-          <Button title="C" onPress={() => this.clearDisplay()} />
-          <Button title="0" onPress={() => this.handleInput('0')} />
-          <Button title="=" onPress={() => this.calculateResult()} />
-        </View>
+
+        <TouchableOpacity>
+          <Text style={styles.butao1}>registrar</Text>
+        </TouchableOpacity>
+        
+
+        <TouchableOpacity>
+          <Text style={styles.butao2}>Ja tens uma conta?</Text>
+        </TouchableOpacity>
+        
+
       </View>
-    );
-  }
-
-  handleInput = (input: string) => {
-    const newDisplayValue =
-      this.state.displayValue === '0' ? input : this.state.displayValue + input;
-    this.setState({ displayValue: newDisplayValue });
-  };
-
-  clearDisplay = () => {
-    this.setState({ displayValue: '0' });
-  };
-
-  calculateResult = () => {
-    try {
-      const result = eval(this.state.displayValue);
-      this.setState({ displayValue: result.toString() });
-    } catch (error) {
-      this.setState({ displayValue: 'Error' });
-    }
-  };
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#5076a5',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  display: {
-    fontSize: 36,
-    marginBottom: 20,
+  cartao:{
+    color:'#fff',
+    marginTop:5,
+    padding:15,
+    height:280,
+    width:220,
+    borderRadius:12,
+    backgroundColor:'#1212',
   },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
+
+  testo:{
+    fontSize:14,
+    fontStyle:'normal',
+    fontWeight:'800'
+  },
+
+  input:{
+    border:0,
+    paddingLeft:9,
+    borderRadius:12,
+    backgroundColor:'#ADD8E6',
+  },
+
+  butao1:{
+    border:0,
+    textAlign:'center',
+    borderRadius:12,
+    paddingBottom:4,
+    backgroundColor:'#ADD8E6',
+    marginTop:10,
+  },
+
+  butao2:{
+    border:0,
+    color:'blue',
+    marginTop:7,
   },
 });
-
-export default CalculatorApp;
